fe1de35609192d7e5106c780f9a92839bc0c23be
Chris270796 <84269447+Chris270796@users.noreply.github.com>
2022-06-02 11:32:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function preload() {}
+
+function draw() {
+
+}
+
+function take_snapshot() {
+    save('student_ice_cream.png');
+}
